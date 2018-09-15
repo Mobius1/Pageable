54ae8a5d42098c7b205d462e71d7326ea672152c
Mobius1 <admin@twitchaddict.tv>
2018-09-15 11:27:12 +0100

    Update DOCS

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,53 +1,123 @@
 const anchors = document.querySelector(".anchors");
 
+
 const pageable = new Pageable("main", {
-	// interval: 400,
-	// delay: 300,
-	// // orientation: "horizontal",
-	// // easing: easings.easeOutBounce,
-	// onInit: init,
-	// onBeforeStart: function(x,y) {
-	// 	console.log("Start:", x,y);
-	// 	this.pages.forEach((page, i) => {
-	// 		page.firstElementChild.classList.remove("active");
-	// 	});	
-	// },
-	// onScroll: function(y) {
-	// 	console.log("Scroll:", Math.round(y));
-	// },
-	// onFinish: function(data) {
-	// 	console.log("Finish:", data);
-	// 	this.pages.forEach((page, i) => {
-	// 		page.firstElementChild.classList.toggle("active", i === this.index);
-			
-	// 		anchors.firstElementChild.children[i].firstElementChild.classList.toggle("active", i === this.index);
-	// 	});	
-	// },
+	onInit: update,
+	onFinish: update,
 });
 
-function init(pages) {
-	const frag = document.createDocumentFragment();
-	
-	pages[0].firstElementChild.classList.add("active");
-	pages.forEach((page, i) => {
-		const id = page.id;
-		const text = `${id.charAt(0).toUpperCase()}${id.substr(1)}`;
+
+const toggle = document.getElementById("settings-open");
+const settings = document.getElementById("settings");
+const inputs = document.querySelectorAll("input");
+const buttons = document.querySelectorAll("button");
+const selects = document.querySelectorAll("select");
+
+document.body.addEventListener("click", e => {
+		settings.classList.toggle("active", settings.contains(e.target));
+});
+
+buttons.forEach(button => {
+	button.onclick = toggleMethod;
+});
+
+inputs.forEach(input => {
+	if ( input.type === "checkbox" ) {
+		input.onchange = toggleEvent;
+	} else {
 		
-		// generate top menu
-		const li = document.createElement("li");
-		const a = document.createElement("a");
+		const output = input.previousElementSibling.lastElementChild;
 		
-		a.href = `#${page.id}`;
+		let config = {
+			tooltips: false,
+			min: 0,
+			step: 100,
+			onInit: val => {
+				output.textContent = `${val}ms`;
+			},			
+			onChange: val => {
+				output.textContent = `${val}ms`;
+			},			
+		};
 		
-		a.textContent = text.replace("-", " ");
-		
-		if ( i === 0 ) {
-			a.classList.add("active");
+		switch(input.id) {
+			case "interval":
+				config.max = 5000;
+				config.value = pageable.config.interval;
+				config.onEnd = val => {
+					pageable.config.interval = val
+				};
+				break;
+			case "delay":
+				config.max = 1000;
+				config.value = pageable.config.delay;
+				config.onEnd = val => { pageable.config.delay = val };
+				break;				
 		}
 		
-		li.appendChild(a);
-		frag.appendChild(li);
-	});
+		new Rangeable(input, config);		
+	}
+});
+
+selects.forEach(select => {
+	initSelect(select);
+});
 	
-	anchors.firstElementChild.appendChild(frag);
+function toggleMethod(e) {
+		if ( "method" in this.dataset ) {
+			pageable[this.dataset.method]();
+		}
+}
+	
+function toggleEvent(e) {
+		if ( "event" in this.dataset ) {
+			pageable.events[this.dataset.event] = this.checked;
+		}
+}
+
+function update() {
+	selects[0].value = pageable.index + 1;
+	selects[1].value = pageable.anchors[pageable.index];
+	selects[2].value = pageable.horizontal ? "horizontal" : "vertical";
+	
+	
+	inputs[0].checked = pageable.events.wheel;
+	inputs[1].checked = pageable.events.mouse;
+	inputs[2].checked = pageable.events.touch;
+}
+
+function initSelect(select) {
+	if ( select.id === "scrollToPage" ) {
+		pageable.pages.forEach((page, i) => {
+			const option = new Option(i+1, i+1);
+			select.add(option);
+		});
+		
+		select.onchange = e => {
+			pageable.scrollToPage(e.target.value);
+			
+			selects[1].value = pageable.anchors[e.target.value - 1];
+		};
+	} else if ( select.id === "scrollToAnchor" ) {
+		pageable.pages.forEach((page, i) => {
+			const option = new Option(`#${page.id}`, `#${page.id}`);
+			select.add(option);
+		});
+		
+		select.onchange = e => {
+			pageable.scrollToAnchor(e.target.value);
+			
+			selects[0].value = pageable.anchors.indexOf(e.target.value) + 1;
+		};		
+	} else if ( select.id === "orientate" ) {
+		
+		["vertical", "horizontal"].forEach(type => {
+			const option = new Option(type, type);
+			select.add(option);
+		});
+		
+		select.onchange = e => {
+			pageable.orientate(e.target.value);
+		};
+	}
 }
