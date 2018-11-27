9ba060b7f303a4cfe3e3603335f2502d3cea5935
Mobius1 <admin@twitchaddict.tv>
2018-11-27 10:55:34 +0000

    Update DOCS

--- docs/js/infinite.js
+++ docs/js/infinite.js
@@ -2,7 +2,4 @@ const pageable = new Pageable("main", {
     freeScroll: true,
     swipeThreshold: 200,
     infinite: true,
-    orientation: "horizontal",
-    navPrevEl: ".nav-prev",
-    navNextEl: ".nav-next",
 });
